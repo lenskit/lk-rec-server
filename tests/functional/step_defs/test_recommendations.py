224e80b47e6a5e905103f8a11c46ce69e99cd788
carlosseguracern <carlosseguracern@u.boisestate.edu>
2020-07-22 20:39:45 -0600

    update docker files
    Former-commit-id: 3bc0fb74f1cca538a360699ae3eaca3568377054

--- tests/functional/step_defs/test_recommendations.py
+++ tests/functional/step_defs/test_recommendations.py
@@ -21,8 +21,9 @@ def http_service(docker_ip, docker_services):
     # `port_for` takes a container port and returns the corresponding host port
     port = docker_services.port_for("rec-server", 5000)
     url = "http://{}:{}/".format(docker_ip, port)
+    print(url)
     docker_services.wait_until_responsive(
-        timeout=30.0, pause=0.1, check=lambda: is_responsive(url)
+        timeout=300.0, pause=0.1, check=lambda: is_responsive(url)
     )
     return url
 
