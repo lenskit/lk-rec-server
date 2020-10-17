40474f9b479ac44790265515befed7fd4c6985ac
Carlos <carlos@carloss-mbp-2.boisestate.edu>
2020-10-16 19:06:11 -0600

    update performance tests and db
    Former-commit-id: d9c0a6729c493256e958aee35343d7cb4fd4d53c

--- test_db/create_tables.sql
+++ test_db/create_tables.sql
@@ -1,4 +1,4 @@
 -- CREATE DATABASE db OWNER postgres;
 
 CREATE TABLE items (id INTEGER PRIMARY KEY, title VARCHAR(200) NOT NULL, genres VARCHAR(100));
-CREATE TABLE rating ("user" INTEGER, item INTEGER, rating FLOAT, timestamp bigint(20), PRIMARY KEY ("user", item));
+CREATE TABLE rating ("user" INTEGER, item INTEGER, rating FLOAT, timestamp bigint, PRIMARY KEY ("user", item));
